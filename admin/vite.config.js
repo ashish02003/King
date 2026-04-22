cf8e01ef3b0835a207b4680be86a31a28c323971
ashish02003 <ashishga2003@gmail.com>
2026-04-22 13:12:44 +0530

    updated deployment

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,10 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/admin/',
+//   build: {
+//     outDir: '../server/public/admin',
+//     emptyOutDir: true,
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/',
+  base: '/',
   build: {
     outDir: '../server/public/admin',
     emptyOutDir: true,
